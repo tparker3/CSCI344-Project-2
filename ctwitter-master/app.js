f635e1d14dd57d7923ede6619640f162120503cd
TParks <tparker3@unca.edu>
2013-02-20 22:07:22 -0500

    added content to index and app'

--- ctwitter-master/app.js
+++ ctwitter-master/app.js
@@ -2,7 +2,7 @@ var main = function () {
     console.log("hello world");
   
     var twitter = new ctwitter.CTwitter();
-    twitter.stream("statuses/filter", { lang:"en", track:["Social Media"] }, function (stream) {
+    twitter.stream("statuses/filter", { lang:"en", track:[""] }, function (stream) {
         stream.on("data", function (tweet) {
               //$("#tweets").append("<img src='"+tweet.profile_image_url+"' />");
              var tweet = $("<p>"+tweet.text+"</p>") 
