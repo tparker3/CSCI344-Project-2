8eedcd7b764d114a2d896ed1f0012db65b69a223
TParks <tparker3@unca.edu>
2013-02-20 20:12:18 -0500

    added ctwitter master'

---
+++ ctwitter-master/app.js
@@ -0,0 +1,21 @@
+var main = function () {
+    console.log("hello world");
+  
+    var twitter = new ctwitter.CTwitter();
+    twitter.stream("statuses/filter", { lang:"en", track:["Social Media"] }, function (stream) {
+        stream.on("data", function (tweet) {
+              //$("#tweets").append("<img src='"+tweet.profile_image_url+"' />");
+             var tweet = $("<p>"+tweet.text+"</p>") 
+            tweet.hide();
+              $("#tweets").append(tweet);
+              console.log(tweet.text);
+               tweet.fadeIn(5000);
+                tweet.fadeOut(5000);
+                  tweet.slideUp();
+                
+        });
+    });
+}
+
+
+$(document).ready(main);
